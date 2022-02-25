893a20eb9991afbe6378cb14ee6b9e5509f9b6df
kelvinsekx <utdkelvin@gmail.com>
2022-02-25 10:56:22 -0800

    addswelcome message

--- src/components/WelcomeBanner.tsx
+++ src/components/WelcomeBanner.tsx
@@ -12,9 +12,11 @@ const WelcomeBanner = ({ close }: TypeClose) => {
                 <h1>Hello World!!, welcome first timer</h1>
                 <div className={styles.flexDiv}>
                     <p className={styles.p}>This is an x and y jungle web game. If you have no idea how to play this awesome game, see the get started video below.</p>
-                    <p>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/Zk0TtOJTA4Q" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-                    </p>
+                    <div>
+                    <iframe
+                    className={styles.frame} 
+                    src="https://www.youtube.com/embed/Zk0TtOJTA4Q" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                    </div>
                 </div>
                 <div>
                     <button
